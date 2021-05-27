3e5c907a89ab99f5cee8277a4dae327054ce22e9
Sezer-dev <s.y.sezeryardim@hotmail.com>
2021-05-28 00:13:38 +0300

    Added textarea-form-component and grouped imported files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
+// Components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CampaignListComponent } from './pages/dashboard/campaign-list/campaign-list.component';
 import { UpdateCampaignModalComponent } from './pages/dashboard/campaign-list/update-campaign-modal/update-campaign-modal.component';
 import { CreateCampaignComponent } from './pages/dashboard/create-campaign/create-campaign.component';
+
+// Shared Components
+import { ButtonComponent } from './pages/shared/button-component/button.component';
+import { TextareaFormComponent } from './pages/shared/textarea-form-component/textarea-form.component';
+
+// Pipes
 import { FilterPipe } from './pipes/filter.pipe';
 import { SortCampaigns } from './pipes/sortCampaigns.pipe';
-import { ButtonComponent } from './pages/shared/button-component/button.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +30,7 @@ import { ButtonComponent } from './pages/shared/button-component/button.componen
     SortCampaigns,
     UpdateCampaignModalComponent,
     ButtonComponent,
+    TextareaFormComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
